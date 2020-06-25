a7f588f677f9e4ee8de339e3ea3158c0dd9dd3a9
xusisi <mixzxu@gmail.com>
2020-06-25 18:14:09 +0800

    Add project structure

---
+++ src/main/java/com/lcss/spikegoodssystem/SpikegoodssystemApplication.java
@@ -0,0 +1,13 @@
+package com.lcss.spikegoodssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikegoodssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpikegoodssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcss/spikegoodssystem/SpikegoodssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcss.spikegoodssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikegoodssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
