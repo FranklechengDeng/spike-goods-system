433fcee66d5e25f0049ad514e798666ef0e4c0ac
xusisi <mixzxu@gmail.com>
2020-06-25 18:36:48 +0800

    Add project structure

--- src/main/java/com/lcss/spikegoodssystem/SpikeGoodsSystemApplication.java
+++ src/main/java/com/lcss/spikegoodssystem/SpikeGoodsSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpikegoodssystemApplication {
+public class SpikeGoodsSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpikegoodssystemApplication.class, args);
+        SpringApplication.run(SpikeGoodsSystemApplication.class, args);
     }
 
 }
